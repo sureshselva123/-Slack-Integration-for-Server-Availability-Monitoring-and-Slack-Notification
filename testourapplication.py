c2b2add39a8b0d3e7c488e8e8436a2ecb49e988d
sureshselva123 <165127899+sureshselva123@users.noreply.github.com>
2024-08-04 19:07:34 +0530

    testourapplication

---
+++ testourapplication.py
@@ -0,0 +1,7 @@
+if not server_status:
+          connection = psycopg2.connect(
+        host='finaldb.cluster-cf062w4wyv9z.ap-northeast-1.rds.amazonaws.com',
+        port=5432,
+        user='sureshdb',
+        password='$Suseela02',
+        database='postgre'
